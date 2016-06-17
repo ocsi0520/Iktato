cea98051d58441c9c8da5d3b52dc932a6f8f68f2
ocsi0520 <ocsi0520@gmail.com>
2016-06-17 14:09:33 +0200

    Add files via upload

---
+++ Iktato_program/Form1.Designer.cs
@@ -0,0 +1,399 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fájlToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mentésToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.megnyitásToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            " - ",
+            "1 - Felnőttek, pedagógia",
+            "2 - Tanulók, pályázatok",
+            "3 - Gazdasági ügyek"});
+            this.comboBox1.Location = new System.Drawing.Point(16, 67);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Enabled = false;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            " - ",
+            "001 - Pedagógus konferencia",
+            "002 - Pályázatok",
+            "003 -Pedagógus határozatok",
+            "004 - Meghívó",
+            "005 - Mérés, értékelés",
+            "006 - KIK, DMJV, vegyes",
+            "007 - Jegyzőkönyvek",
+            "008 - Pedagógus továbbképzések",
+            "009 - Helyettesítés feladás ",
+            "010 - ÖKO iskola",
+            "011 - Kikérő",
+            "012 - Hallgatók",
+            "013 - Könyvtár",
+            "014 - Feljegyzések",
+            "015 - Tantárgyfelosztás",
+            "016 - Felhívás, tájékoztatás, vegyes",
+            "017 - Munkaidőkeret meghatározása, TIK",
+            "018 - Baleseti jegyzőkönyv",
+            "019 - Belső anyag",
+            "020 - Nyíregyházi Főiskola",
+            "021 - Beszámolók",
+            "022 - Oktatási Hivatal (OKÉV)",
+            "023 - Munkaügyi Központ, DEHUSZ",
+            "024 - Munkatervek",
+            "025 - Riadólánc"});
+            this.comboBox2.Location = new System.Drawing.Point(16, 140);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(147, 21);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.Enabled = false;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            " - ",
+            "001 - Egészségügyi felmérés, nev. tanácsadó",
+            "002 - Felvételi tájékoztató (1.o)",
+            "003 - Statisztika",
+            "004 - Tanulmányi verseny,sport",
+            "005 - Kulturális programok",
+            "006 - Egészségügy",
+            "007 - Tanulói okmányok",
+            "008 - Határozatok (köt. tan. óra alól felment.)",
+            "009 - Igazgató helyettes",
+            "010 - Óvoda",
+            "011 - Minőségirányatás",
+            "012 - Azonosító számok, KIR, Diákig",
+            "013 - Tankönyv",
+            "014 - Gyógytestnevelés",
+            "015 - Szülői levelek, határozatok",
+            "016 - Étkezés, határozatok",
+            "017 - Napközi",
+            "018 - Pályázatok",
+            "019 - Meghívók - Ped. Intézet ",
+            "020 - Tanulók érkezése, távozása",
+            "021 - Leendő 1.o/óvodában maradók",
+            "022 - Körzet",
+            "023 - Felvételi (8.o)",
+            "024 - Tehetséges tanulók támogatása",
+            "025 - Taninform",
+            "026 - Tábor",
+            "027 - DÖK",
+            "028 - Büfé",
+            "029 - SODEXHO",
+            "030 - Kikérők, igazolások"});
+            this.comboBox3.Location = new System.Drawing.Point(231, 140);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(147, 21);
+            this.comboBox3.TabIndex = 2;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.Enabled = false;
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            " - ",
+            "001 - Kazinczy",
+            "002 - Költségvetés",
+            "003 - Bérleti szerződés",
+            "004 - Felszólítás, bíróság",
+            "005 - Kifizetés",
+            "006 - Tankönyv elszámolás",
+            "007 - Iskolatej",
+            "008 - Személyi anyag",
+            "009 - Hiányzás jelentés, gyermeknap",
+            "010 - Normatíva (étkezés)",
+            "011 - Felújítás",
+            "012 - Tisztitószer ellátás",
+            "013 - Informatika és szakmai normatíva",
+            "014 - Ellenőrzés",
+            "015 - MÁK",
+            "016 - Megrendelések",
+            "017 - Egyéb",
+            "018 - Fizetési előleg",
+            "019 - Támogatási megállapodás",
+            "020 - Biztosítás",
+            "021 - Gyógytestnevelés normatíva",
+            "022 - Sport normatíva",
+            "023 - Szerződések",
+            "024 - Alapítványi levelek",
+            "025 - Selejtezés leltározás",
+            "026 - Feljegyzések",
+            "027 - Normatíva (iskolai",
+            "028 - Árajánlatok",
+            "029 - Leltározás"});
+            this.comboBox4.Location = new System.Drawing.Point(468, 140);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(147, 21);
+            this.comboBox4.TabIndex = 3;
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Enabled = false;
+            this.label1.Location = new System.Drawing.Point(13, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Felnőttek, pedagógia";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Enabled = false;
+            this.label2.Location = new System.Drawing.Point(228, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Tanulók, pályázatok";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Enabled = false;
+            this.label3.Location = new System.Drawing.Point(465, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Gazdasági ügyek";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Besorolás:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 213);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Berak";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(126, 213);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(351, 164);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(19, 354);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "kimutatás";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fájlToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(831, 24);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fájlToolStripMenuItem
+            // 
+            this.fájlToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mentésToolStripMenuItem,
+            this.megnyitásToolStripMenuItem});
+            this.fájlToolStripMenuItem.Name = "fájlToolStripMenuItem";
+            this.fájlToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fájlToolStripMenuItem.Text = "Fájl";
+            // 
+            // mentésToolStripMenuItem
+            // 
+            this.mentésToolStripMenuItem.Name = "mentésToolStripMenuItem";
+            this.mentésToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
+            this.mentésToolStripMenuItem.Text = "Mentés";
+            this.mentésToolStripMenuItem.Click += new System.EventHandler(this.mentésToolStripMenuItem_Click);
+            // 
+            // megnyitásToolStripMenuItem
+            // 
+            this.megnyitásToolStripMenuItem.Name = "megnyitásToolStripMenuItem";
+            this.megnyitásToolStripMenuItem.Size = new System.Drawing.Size(129, 22);
+            this.megnyitásToolStripMenuItem.Text = "Megnyitás";
+            this.megnyitásToolStripMenuItem.Click += new System.EventHandler(this.megnyitásToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(515, 213);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(277, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(515, 282);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(277, 20);
+            this.textBox2.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(512, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Név (nem szükséges)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(512, 266);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(145, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Megjegyzés (nem szükséges)";
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(515, 314);
+            this.monthCalendar1.MaxDate = new System.DateTime(2209, 12, 31, 0, 0, 0, 0);
+            this.monthCalendar1.MaxSelectionCount = 1;
+            this.monthCalendar1.MinDate = new System.DateTime(1800, 1, 1, 0, 0, 0, 0);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 17;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(618, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Keresés";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(831, 547);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Iktató Program";
+            this.Text = "Iktató Program";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fájlToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mentésToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem megnyitásToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Iktato_program/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        int besorolas = 0, alszam = 0;
+        List<sor> lista = new List<sor>();
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+            comboBox3.SelectedIndex = 0;
+            comboBox4.SelectedIndex = 0;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            besorolas = comboBox1.SelectedIndex;
+            switch (besorolas)
+            {
+                case 0:
+                    comboBox2.Enabled = false;
+                    comboBox3.Enabled = false;
+                    comboBox4.Enabled = false;
+                    label1.Enabled = false;
+                    label2.Enabled = false;
+                    label3.Enabled = false;
+                    comboBox2.SelectedIndex = 0;
+                    comboBox3.SelectedIndex = 0;
+                    comboBox4.SelectedIndex = 0;
+                    alszam = 0;
+                    break;
+                case 1:
+                    comboBox2.Enabled = true;
+                    comboBox3.Enabled = false;
+                    comboBox4.Enabled = false;
+                    label1.Enabled = true;
+                    label2.Enabled = false;
+                    label3.Enabled = false;
+                    comboBox2.SelectedIndex = 0;
+                    comboBox3.SelectedIndex = 0;
+                    comboBox4.SelectedIndex = 0;
+                    break;
+                case 2:
+                    comboBox2.Enabled = false;
+                    comboBox3.Enabled = true;
+                    comboBox4.Enabled = false;
+                    label1.Enabled = false;
+                    label2.Enabled = true;
+                    label3.Enabled = false;
+                    comboBox2.SelectedIndex = 0;
+                    comboBox3.SelectedIndex = 0;
+                    comboBox4.SelectedIndex = 0;
+                    break;
+                case 3:
+                    comboBox2.Enabled = false;
+                    comboBox3.Enabled = false;
+                    comboBox4.Enabled = true;
+                    label1.Enabled = false;
+                    label2.Enabled = false;
+                    label3.Enabled = true;
+                    comboBox2.SelectedIndex = 0;
+                    comboBox3.SelectedIndex = 0;
+                    comboBox4.SelectedIndex = 0;
+                    break;
+                default:
+                    MessageBox.Show("Vmi gebasz van");
+                    break;
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            alszam = comboBox2.SelectedIndex;
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            alszam = comboBox3.SelectedIndex;
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            alszam = comboBox4.SelectedIndex;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (besorolas == 0 || alszam == 0)
+                MessageBox.Show("Gebasz van! Minden mezőben választani kell!");
+            else
+            {
+                richTextBox1.Text = "Eddig " + lista.Count(x => x.al == alszam && x.besor == besorolas) + " van belőle, így ez a " + (lista.Count(x => x.al == alszam && x.besor == besorolas)+1);
+                lista.Add(new sor(besorolas, alszam, textBox1.Text, textBox2.Text, monthCalendar1.SelectionRange.Start.ToShortDateString()));
+                textBox1.Text="";
+                textBox2.Text="";
+                monthCalendar1.SelectionStart= DateTime.Now;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            foreach (var item in lista.OrderBy(x=>x.besor).GroupBy(x=>x.besor))
+            {
+                richTextBox1.Text += "Besorolás alapján: " + item.First().besor.ToString("000") + "\n";
+                foreach (var item2 in item.Select(x=>x.al).Distinct().OrderBy(x=>x))
+                {
+                    richTextBox1.Text += item2.ToString("000") + " - " + item.Count(x=>x.al==item2) + " db\n";
+                }
+            }
+        }
+
+        private void mentésToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sf = new SaveFileDialog();
+            sf.DefaultExt = "txt";
+            sf.ShowDialog();
+            if (sf.FileName != "")
+            {
+                List<string> vmi = new List<string>();
+                foreach (sor item in lista)
+                {
+                    vmi.Add(item.besor + "|" + item.al + "|" + item.nev + "|" + item.megjegyzes + "|" + item.datum);
+                }
+                File.WriteAllLines(sf.FileName, vmi);
+            }
+        }
+
+        private void megnyitásToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("A most használt adatok elvesznek, ha újat nyitsz meg", "FIGYELEM!!!");
+            OpenFileDialog of = new OpenFileDialog();
+            of.ShowDialog();
+            if (of.FileName != "")
+            {
+                sor.szamlalo = 3063;
+                lista.Clear();
+                foreach (string item in File.ReadAllLines(of.FileName))
+                {
+                    string[] asd = item.Split('|');
+                    lista.Add(new sor(Convert.ToInt32(asd[0]), Convert.ToInt32(asd[1]),asd[2],asd[3],asd[4]));
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            List<sor> seged = lista.Where(x => x.nev.ToLower().Contains(textBox1.Text.ToLower()) && x.megjegyzes.ToLower().Contains(textBox2.Text.ToLower())).ToList();
+            if (besorolas != 0)
+            {
+                seged = seged.Where(x => x.besor == besorolas).ToList();
+                if (alszam != 0)
+                    seged = seged.Where(x => x.al == alszam).ToList();
+            }
+            if (seged.Count == 0)
+            {
+                richTextBox1.Text = "Nem található ilyen objektum"; return;
+            }
+            richTextBox1.Text = "Besorolás | Alszám | Név | Megjegyzés | Dátum | Sorszám\n\n";
+            foreach (sor item in seged)
+            {
+                richTextBox1.Text += item.besor.ToString("000") + " | " + item.al.ToString("000") + " | " + item.nev + " | " + item.megjegyzes + " | " + item.datum + " | " + item.sorszam + "\n";
+            }
+        }
+    }
+    class sor
+    {
+        public static int szamlalo = 3063;
+        public int besor, al, sorszam;
+        public string nev, megjegyzes,datum;
+        public sor(int besor, int al,string nev,string megjegyzes,string datum)
+        {
+            this.besor = besor;
+            this.al = al;
+            if (nev == "")
+                this.nev = "-";
+            else
+            this.nev = nev;
+            if (megjegyzes == "")
+                this.megjegyzes = "-";
+            else
+            this.megjegyzes = megjegyzes;
+            this.datum = datum;
+            this.sorszam = ++sor.szamlalo;
+        }
+    }
+}
